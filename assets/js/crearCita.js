c1597ffd5f64817569c9addf4b1dc06492d87b97
Erickr2 <erickurr0312@gmail.com>
2021-08-13 22:49:48 -0500

    ADD controlador.. controlador para la logica de la vista crearCita

---
+++ assets/js/crearCita.js
@@ -0,0 +1,45 @@
+import { API } from './api'
+
+const $nombre = document.getElementById("nombre");
+const $apellidom = document.getElementById("ramirez");
+const $apellidop = document.getElementById("ramirez");
+const $consultorio = document.getElementById("consultorio");
+const $medico = document.getElementById("medico");
+const $fecha = document.getElementById("fecha");
+const $hora = document.getElementById("hora");
+
+
+const validateUser = async(event, intentos = 1) => {
+    event.preventDefault();
+    console.log($nombre.value);
+    console.log($apellidom.value);
+
+    //TODO: VALIDAR USUARIO Y CONTRASEÑA VS BD
+    /** 
+    try {
+        const params = {
+            method: "POST",
+            mode: "cors",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                nombre: $nombre.value,
+                apellidom: $apellidom.value,
+                apellidop: $apellidop.value,
+                consultorio: $consultorio.value,
+                medico: $medico.value,
+                fecha: $fecha.value,
+                hora: $hora.value
+
+            }),
+        };
+        const res = await fetch(API, params);
+        const data = await res.json();
+        console.log(data);
+    } catch (error) {
+        console.error(error);
+*/
+}
+
+};
+
+$btnLogin.onclick((e) => validateUser(e));
